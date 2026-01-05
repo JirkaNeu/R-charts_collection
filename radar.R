b257048bac1cec42f72891904886a8c18a3ee9b9
Jirka Neumann <jirka.neumann@gmx.de>
2026-01-05 22:41:06 +0100

    update radar

--- radar.R
+++ radar.R
@@ -1,33 +1,52 @@
 library(fmsb)
 
 
-touren = c(
+Daten = c(
   Berlin = 7,
   Hamburg = 8,
   Frankfurt = 4,
   Köln = 12,
-  München = 2
-  )
+  München = 2,
+  Dresden = 15
+)
+
+Daten2 = c(
+  Berlin = 3,
+  Hamburg = 12,
+  Frankfurt = 7,
+  Köln = 10,
+  München = 8,
+  Dresden = 5
+)
 
 
-all_max = sum(touren)
+
+#all_max = sum(Daten)
+all_max = max(Daten) + 2
 all_min = 0
 
-all_t_df = as.data.frame(rbind(all_max, all_min, touren))
+all_d_df = as.data.frame(rbind(all_max, all_min, Daten))
 
+all_d_df = rbind(all_d_df, Daten2)
 
+farben = c(rgb(0, 0.4, 1, 0.4),
+           rgb(1, 0, 0.5, 0.4))
 
-
-
-radarchart(all_t_df,
+radarchart(all_d_df,
            cglty = 1,       # Grid line type
            cglcol = "gray", # Grid line color
            cglwd = 1,       # Grid line width
            plty = 1,        # Area line type
            pcol = 4,        # Area line color
            plwd = 2,        # Area line Width
-           pfcol = rgb(0, 0.4, 1, 0.25)        # Area line fill and alpha
-           )
+           pfcol = farben   # Area line fill and alpha
+)
+
+legend("right",
+       legend = c("Daten früher", "Daten später"),
+       bty = "n", pch = 20, col = farben,
+       text.col = "grey25", pt.cex = 2)
+
 
 #2do:
 #*try cairo-Pac
